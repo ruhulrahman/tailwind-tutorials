1474ad3c6520f5218c2e8b746820415b00320375
ruhulrahman2233 <ruhulrahman2233@gmail.com>
2024-11-22 19:35:14 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    // Example content paths...
+    // './public/**/*.html',
+    // './src/**/*.{js,jsx,ts,tsx,vue}',
+    // "./src/**/*.{html,js}",
+    "./*.{html,js}"
+  ],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
